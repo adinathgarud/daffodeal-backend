fc97027990562ad2e8bb8809c53c0239a03c6a3a
adinathgarud <adinathgarud123@gmail.com>
2025-02-19 18:42:44 +0530

    origin Update

--- app.js
+++ app.js
@@ -10,10 +10,7 @@ const cors = require("cors");
 //   credentials: true
 // }));
 
-app.use(cors({
-    origin: [],
-    credentials: true
-  }));
+app.use(cors());
 
 app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
