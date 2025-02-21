a47c58af5ecf43611e935b9cbc0678a794f21bbe
adinathgarud <adinathgarud123@gmail.com>
2025-02-21 16:23:16 +0530

    origin new 2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-app.use(cors());
+//app.use(cors());
 // app.use(cors({
 //   origin: ['http://localhost:3000',],
 //   credentials: true
